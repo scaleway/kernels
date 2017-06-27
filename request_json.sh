d7f80ce02d3c0089e101bfc2fd9243e6b33cba0f
Jérôme Tamba <jtamba@online.net>
2017-06-27 16:09:07 +0200

    Simplify data to send

--- request_json.sh
+++ request_json.sh
@@ -14,19 +14,10 @@ urlencode() {
     echo $(python3 -c 'import sys, urllib.parse; print(urllib.parse.quote(sys.argv[1], safe=""))' $1)
 }
 
-kernel_flavor=${branch%%/*}
 enc_branch=$(urlencode $branch)
 encenc_branch=$(urlencode $enc_branch)
 
-build_path="/job/kernel-build/job/$encenc_branch/$buildno"
-artifacts_url="${JENKINS_URL}${build_path}/artifact/"
-kernel_version=$(curl -s $artifacts_url/$arch/release/version)
-if [ "$arch" = 'arm' -o "$arch" = 'x86_64' ]; then
-    image_type='vmlinuz'
-elif [ "$arch" = 'arm64' ]; then
-    image_type='vmlinux'
-fi
-kernel_name="${image_type}-${kernel_version}"
+build_path="/job/kernel-build/job/$encenc_branch/$buildno/artifact/$arch/release"
 
-jq -n --arg t $is_test --arg kf "$kernel_flavor" --arg kv "$kernel_version" --arg kn "$kernel_name" --arg bp "$build_path" --arg a "$arch" \
-    '{ type: "kernel", test: ($t == "true"), data: { kernel_flavor: $kf, kernel_version: $kv, kernel_name: $kn, build_path: $bp, arch: $a } }'
+jq -n --arg t $is_test --arg p "$build_path" --arg a "$arch" --arg b "$branch" \
+    '{ type: "kernel", test: ($t == "true"), data: { build_path: $p, arch: $a, branch: $b } }'
