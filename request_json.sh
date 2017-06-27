22a287459999ea784fcc4611326233d53629a534
Jérôme Tamba <jtamba@online.net>
2017-06-27 17:46:54 +0200

    Change JSON field name

--- request_json.sh
+++ request_json.sh
@@ -17,7 +17,7 @@ urlencode() {
 enc_branch=$(urlencode $branch)
 encenc_branch=$(urlencode $enc_branch)
 
-build_path="/job/kernel-build/job/$encenc_branch/$buildno/artifact/$arch/release"
+release_path="/job/kernel-build/job/$encenc_branch/$buildno/artifact/$arch/release"
 
-jq -n --arg t $is_test --arg p "$build_path" --arg a "$arch" --arg b "$branch" \
-    '{ type: "kernel", test: ($t == "true"), data: { build_path: $p, arch: $a, branch: $b } }'
+jq -n --arg t $is_test --arg p "$release_path" --arg a "$arch" --arg b "$branch" \
+    '{ type: "kernel", test: ($t == "true"), data: { release_path: $p, arch: $a, branch: $b } }'
